9ed25ac6508b5c75f5de569dd25d79212587aff1
silenarestrepo <restreposilena@gmail.com>
2020-06-10 22:56:55 +0000

    c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@ double doubleType;
 char charType;
 
 // sizeof evaluates the size of a variable
-printf("Size of int: %lu bytes\n", (long init) sizeof(i));
-printf("Size of float: %lu byte(s)\n", (long init) sizeof(f));
-printf("Size of double: %lu bytes\n", (long init) sizeof(d));
-printf("Size of char: %lu byte(s)\n", (long int) sizeof(c));
+printf("Size of int: %lu bytes\n", sizeof(i));
+printf("Size of float: %lu byte(s)\n", sizeof(f));
+printf("Size of double: %lu bytes\n", sizeof(d));
+printf("Size of char: %lu byte(s)\n", sizeof(c));
     
 return 0;
 }
