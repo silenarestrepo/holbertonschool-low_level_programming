0552f2b22a198e319e6f55a2d91558d78296c69a
silenarestrepo <restreposilena@gmail.com>
2020-06-10 22:24:06 +0000

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
 /**
- * main - use printf typeof
- * Description: output with printf
- * Return: 0
+ *main - Prints a specified string using printf
+ *
+ *Description: The function will print the string "with proper grammar, but the
+ *outcome is a piece of art," using the printf function.
+ *Return: 0 if the function ran to completion
  */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
