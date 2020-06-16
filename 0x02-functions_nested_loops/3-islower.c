c82009131431ccc6742753586558f8b183129dbd
silenarestrepo <restreposilena@gmail.com>
2020-06-16 19:00:07 +0000

    fuctions

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include holberton.h
+/**
+ * _islower - write a function that chfecks for lowercase characters.
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
