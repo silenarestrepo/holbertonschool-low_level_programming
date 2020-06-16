6938947d52bc201f51a377616b7e2bff4bc1e29f
silenarestrepo <restreposilena@gmail.com>
2020-06-16 23:24:59 +0000

    functions

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+
 /**
  * _abs - takes number and returns absolute value of that number
  * @i: number to be check
@@ -7,12 +8,12 @@
  */
 int _abs(int i)
 {
-if  (i >= 0)
-	{
-		return (i);
-	}
-	else
-	{
-		return (0 - i);
-	}
+if (i >= 0)
+{
+return (i);
+}
+else
+{
+return (0 - i);
+}
 }
