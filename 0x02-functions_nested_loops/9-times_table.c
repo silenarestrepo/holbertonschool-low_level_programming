80de39d842f5f3064a0a86cd91600affbb74fcda
silenarestrepo <restreposilena@gmail.com>
2020-06-17 02:04:38 +0000

    functions

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,36 +2,36 @@
 
 /**
  * times_table - prints 9x table
- * Description - time tables
+ *
  * Return: void
  */
 void times_table(void)
 {
-int column, row, sum, t;
+	int column, row, sum, t;
 
-column = 0;
-while (column < 10)
-{
-_putchar('0');
-row = 1;
-while (row < 10)
-{
-_putchar(',');
-_putchar(' ');
-sum = column *row, t = sum / 10;
-if (sum < 10)
-{
-_putchar(' ');
-_putchar(sum + '0');
-}
-else
-{
-_putchar(t + '0');
-_putchar(sum % 10 + '0');
-}
-row++;
-}
-_putchar('\n');
-column++;
-}
+	column = 0;
+	while (column < 10)
+	{
+		_putchar('0');
+		row = 1;
+		while (row < 10)
+		{
+			_putchar(',');
+			_putchar(' ');
+			sum = column * row, t = sum / 10;
+			if (sum < 10)
+			{
+				_putchar(' ');
+				_putchar(sum + '0');
+			}
+			else
+			{
+				_putchar(t + '0');
+				_putchar(sum % 10 + '0');
+			}
+			row++;
+		}
+		_putchar('\n');
+		column++;
+	}
 }
