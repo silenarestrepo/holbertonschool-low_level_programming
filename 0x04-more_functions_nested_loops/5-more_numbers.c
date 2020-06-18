844297428f2ebbea78da0925819a364c5b9752a9
silenarestrepo <restreposilena@gmail.com>
2020-06-18 18:40:28 +0000

    nested loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,21 +1,22 @@
 #include "holberton.h"
 
 /**
- * more_numbers - prints x10 numbers from 0 to 14 followed by a new line
- *
+ * more_numbers - prints number from 0 to 14
+ * followd by a new line 10 times
  */
-
 void more_numbers(void)
 {
-char num;
-int tentimes;
+	char num;
+	int TenTimes;
 
-for (tentimes = 48; tentimes <= 57; tentimes++)
-{
-for (num <= 10)
-_putchar(num / 10 + '0');
-_putchar(num % 10 + '0');
-}
-_putchar ('\n');
-}
+	for (TenTimes = 48; TenTimes <= 57; TenTimes++)
+	{
+		for (num = 0; num < 15; num++)
+		{
+			if (num >= 10)
+				_putchar(num / 10 + '0');
+			_putchar (num % 10 + '0');
+		}
+		_putchar ('\n');
+	}
 }
