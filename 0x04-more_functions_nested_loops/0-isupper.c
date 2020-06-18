e328a7b4f99bc67761774f1cc79b60e86f308311
silenarestrepo <restreposilena@gmail.com>
2020-06-18 17:04:18 +0000

    nested loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_isupper - check for uppercase characters.
+ * @c - character to be checked
+ * Return 1 if c is uppercase and 0 otherwise.
+ */
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
