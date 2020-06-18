5264a891b07f747b6e2fcc02bd6bb9ba54be1302
silenarestrepo <restreposilena@gmail.com>
2020-06-18 18:02:06 +0000

    nested loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers -  print the numbers from 0 to 9 except for 2 and 4 followed by a new line.
+ *
+ */
+void print_most_numbers(void)
+{
+char num;
+for (num=48; num <=57; num++)
+if (num != 50 && num != 52)
+{
+_putchar (num);
+}
+_putchar ('\n');
+}
