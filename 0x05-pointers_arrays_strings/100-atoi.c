8f6265b0110be8cb950a9007c62ae09e56e8b4da
silenarestrepo <restreposilena@gmail.com>
2020-06-23 18:44:38 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * is_num - checks if it is a digit
+ * @n: Number
+ * Return: If it is a number, retrun 1 else return 0
+ */
+int is_num(unsigned int n)
+{
+	return (n >= '0' && n <= '9');
+}
+
+/**
+ * _atoi - convert a string to a number
+ * @s: String to be converted
+ * Return: returns the number
+ */
+int _atoi(char *s)
+{
+	unsigned int num, i;
+	int sign;
+
+	sign = 1;
+	num = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (is_num(s[i]))
+
+		{
+			num = (s[i] - '0') + num * 10;
+		}
+		if (s[i + 1] == ' ')
+		{
+			break;
+		}
+		else if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+	}
+	return (num * sign);
+}
