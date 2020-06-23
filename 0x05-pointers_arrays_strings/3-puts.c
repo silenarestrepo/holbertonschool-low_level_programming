d518df03fb7679f6bd0a1cfce18df8cabcf7977d
silenarestrepo <restreposilena@gmail.com>
2020-06-23 15:44:21 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _puts: print a string followed by a new line
+ * @str: variable that contains the string
+ */
+void _puts(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
+	_putchar('\n');
+}
