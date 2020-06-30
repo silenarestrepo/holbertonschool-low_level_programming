632ad53a28e12537c17b34e50ab3e58f78b93caa
silenarestrepo <restreposilena@gmail.com>
2020-06-30 02:29:08 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _strspn - calculates the length of the initial segment
+ * of str1 which consists entirely of characters in str2.
+ * @s: The main string to be scanned.
+ * @accept: The list of characters to match in s.
+ *
+ * Return: return the length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+}
