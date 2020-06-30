9a019199a3729c858ae314a9a72d1fdfa4f23071
silenarestrepo <restreposilena@gmail.com>
2020-06-30 02:32:49 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - Search the first occurence of any of the bytes in *accept.
+ * @s: Travel this string.
+ * @accept: bytes to read the occurence in s.
+ *
+ * Return: return the first occurence.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+}
