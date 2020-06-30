8bff901e06526bca7d3f4b620adefccb85132ae9
silenarestrepo <restreposilena@gmail.com>
2020-06-30 02:21:34 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _memcpy - copy n bytes from src to dest
+ * @dest: destination pointer
+ * @src: source pointer
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
