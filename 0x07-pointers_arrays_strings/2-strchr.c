54825104b9f25ab1c7bae4075081dff0499c97b2
silenarestrepo <restreposilena@gmail.com>
2020-06-30 02:24:24 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _strchr - locates the 1st occurence of character c.
+ * @s: String to travel.
+ * @c: Character to locate.
+ *
+ * Return: The first occurence of a character
+ */
+char *_strchr(char *s, char c)
+{
+
+}
