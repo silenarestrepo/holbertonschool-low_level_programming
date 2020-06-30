8096a2a2aff7e2ac13c89f95d5ccdeb51aa25367
silenarestrepo <restreposilena@gmail.com>
2020-06-30 02:36:26 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * comp - Travels in every content for x and y, then returns 1/0.
+ * @c1: string 1.
+ * @c2: string 2
+ * Return: return 1 if comparing is true.
+ */
+int comp(char *c1, char *c2)
+{
+
+}
+/**
+ * _strstr - takes two strings and find occurences of substring.
+ * @haystack: string
+ * @needle: substring
+ * Return: This function returns a pointer points
+ *         to the first character of the found s2 in s1.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
+}
