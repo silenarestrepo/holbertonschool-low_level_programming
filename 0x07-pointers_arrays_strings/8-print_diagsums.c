e0766719f15650323956bd92606514dcfe8008c1
silenarestrepo <restreposilena@gmail.com>
2020-06-30 02:47:39 +0000

    pointers

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - Prints diagonal sums of a matrix [IxI]
+ * @a: Pointer to the matrix [row][columns]
+ * @size: Size of one the string one dimension.
+ */
+void print_diagsums(int *a, int size)
+{
+
+}
