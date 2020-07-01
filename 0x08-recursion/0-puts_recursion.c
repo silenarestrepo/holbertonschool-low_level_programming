d521ba051a71c6c54af52a8b4e41bd10973ea30e
silenarestrepo <restreposilena@gmail.com>
2020-07-01 20:38:30 +0000

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - print a string followed by a new line
+ * @s: string
+ */
+
+void _puts_recursion(char *s)
+{
+if (*s)
+_putchar(*s), _puts_recursion(s + 1);
+else
+_putchar('\n');
+}
---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+#include <unistd.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+
+
+#endif /* HOLBERTON_H */
