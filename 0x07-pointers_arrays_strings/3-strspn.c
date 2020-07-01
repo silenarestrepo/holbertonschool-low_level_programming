d9e6a8efd7a542c7d434308daf3c92f77084671a
silenarestrepo <restreposilena@gmail.com>
2020-07-01 23:58:26 +0000

    recursion

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,5 +9,15 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
+unsigned int s_count, a_count;
 
+	for (s_count = 0; s[s_count] != '\0'; s_count++)
+	{
+		for (a_count = 0; accept[a_count] != '\0'; a_count++)
+			if (s[s_count] == accept[a_count])
+				break;
+		if (accept[a_count] == '\0')
+			break;
+	}
+	return (s_count);
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,4 +11,13 @@
 char *_strpbrk(char *s, char *accept)
 {
 
+	unsigned int s_count, a_count;
+
+	for (s_count = 0; s[s_count] != '\0'; s_count++)
+	{
+		for (a_count = 0; accept[a_count] != '\0'; a_count++)
+			if (s[s_count] == accept[a_count])
+				return (&s[s_count]);
+	}
+	return (0);
 }
