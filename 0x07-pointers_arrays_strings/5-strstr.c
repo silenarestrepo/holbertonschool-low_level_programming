c5d3079e7b3b311919b06e6c0556c23d38ff02d9
silenarestrepo <restreposilena@gmail.com>
2020-07-02 00:01:11 +0000

    recursion

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,22 +1,35 @@
 #include "holberton.h"
 /**
- * comp - Travels in every content for x and y, then returns 1/0.
- * @c1: string 1.
- * @c2: string 2
- * Return: return 1 if comparing is true.
- */
-int comp(char *c1, char *c2)
-{
-
-}
-/**
  * _strstr - takes two strings and find occurences of substring.
  * @haystack: string
  * @needle: substring
- * Return: This function returns a pointer points
- *         to the first character of the found s2 in s1.
+ * Return: s
  */
 char *_strstr(char *haystack, char *needle)
 {
 
+	unsigned int h_count = 0, n_count = 0, count = 0;
+
+	while (haystack[h_count] != '\0' && needle[n_count] != '\0')
+	{
+		if (haystack[h_count] != needle[n_count])
+		{
+			h_count++;
+			n_count = 0;
+			if (count > 0)
+				h_count -= count;
+			count = 0;
+		}
+		else
+		{
+			h_count++;
+			n_count++;
+			count++;
+			if (needle[n_count] == '\0')
+				return (&haystack[h_count - count]);
+		}
+	}
+	if (needle[0] == '\0')
+		return (haystack);
+	return (NULL);
 }
