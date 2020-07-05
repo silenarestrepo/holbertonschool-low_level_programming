da9252f863a1203ce3d62c4bae3b3295a7c355e8
silenarestrepo <restreposilena@gmail.com>
2020-07-05 04:03:02 +0000

    static libraries

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
-
+#include "holberton.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * _puts - print a string followed by a new line
+ * @s: variable that contains the string
  */
-
-int main(void)
+void _puts(char *s)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	int a;
+
+	a = 0;
+	while (s[a] != '\0')
+	{
+		_putchar(s[a]);
+		a++;
+	}
+	_putchar('\n');
 }
