27913123eb6aff6d83c28788ac797d2cd9edd57e
silenarestrepo <restreposilena@gmail.com>
2020-07-06 02:01:53 +0000

    arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: count the size of argv
+ * @argv: array that contains the arguments
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+#include <unistd.h>
+
+int _putchar(char c);
+
+
+
+#endif /* HOLBERTON_H */
