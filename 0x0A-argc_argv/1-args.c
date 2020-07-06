d3dd642dd6caad13430835f997d3a636a2b816be
silenarestrepo <restreposilena@gmail.com>
2020-07-06 02:04:33 +0000

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: size of argv string
+ * @argv: array of strings
+ * Return: 0
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
