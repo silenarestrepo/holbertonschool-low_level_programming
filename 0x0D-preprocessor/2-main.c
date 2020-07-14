0568100dab4e96bd1f1ba3fdc9eb6464fe3b8879
silenarestrepo <restreposilena@gmail.com>
2020-07-14 00:06:00 +0000

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - print the name of a file
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
---
+++ 0x0D-preprocessor/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+#include <stdio.h>
+
+int _putchar(char c);
+int main(void);
+
+
+#endif
