1d72b674c602aba389e3302a2a04a58e67a0725b
silenarestrepo <restreposilena@gmail.com>
2020-07-18 21:01:01 +0000

    variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -26,7 +26,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	printf("%d", va_arg(ab, int));
 
-	if (i < (n - 1))
+	if (i != (n - 1) && separator != NULL)
 	printf("%s", separator);
 
 	}
