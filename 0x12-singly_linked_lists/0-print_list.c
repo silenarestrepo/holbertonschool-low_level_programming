665fbcae21260c9649ad890480596cf6b4ad07ad
silenarestrepo <restreposilena@gmail.com>
2020-07-23 22:31:02 +0000

    linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_list - prints elements of a list
- * @h: header of the list
- * Return: number of nodes
+ * print_list - prints all the elements of a list_t list
+ * @h: head
+ * Return: the number of nodes
  */
 
 size_t print_list(const list_t *h)
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	{
 		if (h->str == NULL)
 		{
-		printf("[0] (nil)\n");
+			printf("[0] (nil)\n");
 		}
 		else
 		{
