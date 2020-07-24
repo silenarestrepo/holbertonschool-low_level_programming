32529a00e848aeda38d6748261ec4cf68d3e2b9c
silenarestrepo <restreposilena@gmail.com>
2020-07-24 02:21:28 +0000

    linked lists

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at a beginning of a list
+ * @head: double pointer
+ * @str: string to be duplicated
+ * Return: address of the new element and NULL otherwise
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	int i;
+	list_t *new_element;
+
+	i = 0;
+
+	if (!str)
+		return (NULL);
+
+	new_element = malloc(sizeof(list_t));
+	if (new_element == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != 0; i++)
+
+	new_element->str = strdup(str);
+	new_element->len = i;
+	new_element->next = *head;
+
+	*head = new_element;
+
+	return (new_element);
+}
