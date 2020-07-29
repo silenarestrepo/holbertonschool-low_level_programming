66a3794393c979576d40919613b8975ce8922108
silenarestrepo <restreposilena@gmail.com>
2020-07-29 02:02:31 +0000

    more singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,6 +3,7 @@
 /**
  * free_listint2 - frees a list when function sets head to NULL
  * @head: head of the list
+ * Return: nothing
  */
 void free_listint2(listint_t **head)
 {
@@ -11,11 +12,9 @@ void free_listint2(listint_t **head)
 	if (head == NULL)
 		return;
 
-	while (head != NULL)
 	{
 		p = (*head)->next;
 		free(*head);
 		*head = p;
 	}
-	head = NULL;
 }
