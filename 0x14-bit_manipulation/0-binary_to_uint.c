290583ed3fa023826276ba23f2509798a951a5b3
silenarestrepo <restreposilena@gmail.com>
2020-07-30 22:40:56 +0000

    bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,17 +11,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 
 	if (b == NULL)
-	{
 		return (0);
-	}
 
 	while (b[i])
 	{
 		num = num << 1;
-		if (b[i] != '0' && b[i] != '1')
-		{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
+		else if (b[i] == '1')
+			num = num | 1;
 		i++;
 	}
 
