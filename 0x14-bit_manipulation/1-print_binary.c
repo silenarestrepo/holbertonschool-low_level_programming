f8e4eb18890c246a9d2277b1c2ba860d60c510a2
silenarestrepo <restreposilena@gmail.com>
2020-07-31 00:22:54 +0000

    bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_binary - prints a number as binary
+ * @n: decimal number
+ */
+void print_binary(unsigned long int n)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	else if (n == 1)
+	{
+		_putchar('1');
+		return;
+	}
+}
