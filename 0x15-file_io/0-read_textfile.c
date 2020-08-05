47c23956366da96d62d6f0de4dc27605f5eabc7d
silenarestrepo <restreposilena@gmail.com>
2020-08-05 19:49:14 +0000

    code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,37 +6,46 @@
  * @filename: name of the file
  * Return: number of letters it could read and print and 0 otherwise
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
+	ssize_t fwd, a;
 	char *buf;
-	ssize_t num, wr;
 
 	if (filename == NULL)
 		return (0);
 
+	if (letters == 0)
+		return (0);
+
 	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+
+	if (buf == NULL)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
-	num = read(fd, buf, sizeof(char) * letters);
-	buf[num] = '\0';
-	close(fd);
+	fwd = read(fd, buf, letters);
 
-	if (num == -1)
+	if (fwd == -1)
 	{
 		free(buf);
 		return (0);
 	}
 
-	wr = (write(STDOUT_FILENO, buf, num));
-	if (wr == -1)
-		wr = 0;
-	free(buf);
+	close(fd);
 
-	return (wr);
+	a = write(STDOUT_FILENO, buf, fwd);
+
+	if (a == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (a);
 }
