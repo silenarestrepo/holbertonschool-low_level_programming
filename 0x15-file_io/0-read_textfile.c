b1557dd05ac897cf61a3610adfe96621004328dc
silenarestrepo <restreposilena@gmail.com>
2020-08-05 01:49:12 +0000

    file i/o

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - reads a text file
+ * @letters: number of letter it should read and print
+ * @filename: name of the file
+ * Return: number of letters it could read and print and 0 otherwise
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	ssize_t num;
+
+	if (filename == NULL)
+		return (0);
+
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	num = read(fd, buf, letters);
+
+	close(fd);
+	return (write(1, buf, num));
+}
