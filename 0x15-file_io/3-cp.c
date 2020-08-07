0c9c0c40669f87b7ff3474a14af9433cd71e7817
silenarestrepo <restreposilena@gmail.com>
2020-08-07 02:55:09 +0000

    code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,8 @@ int main(int argc, char **argv)
 			exit(99);
 
 	f_from = open(argv[1], O_RDONLY);
-	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	f_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC,
+		    S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	read_value = read(f_from, buf, 1024);
 	if (read_value == -1)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
