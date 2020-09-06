4d943fedd6e9f1d8cc6609edf2851191a32b119e
silenarestrepo <restreposilena@gmail.com>
2020-09-06 22:48:38 +0000

    task 0 and 1 dll

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,11 +7,11 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-    int i;
+	int i;
 
-    for (i = 0; h != NULL; i++)
-    {
-        h = h->next;
-    }
-    return (i);
+	for (i = 0; h != NULL; i++)
+	{
+		h = h->next;
+	}
+	return (i);
 }
