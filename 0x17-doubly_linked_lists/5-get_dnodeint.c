114bdccfa77e83dd87340d302ddffb320f83ff76
silenarestrepo <restreposilena@gmail.com>
2020-09-07 03:06:36 +0000

    task 5 dll

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,21 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index -
- *
- * Return: 
+ * get_dnodeint_at_index -  returns the nth node of a dlinked list
+ * @head: head of the list
+ * @index: index
+ * Return: the nth node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	unsigned int i = 0;
 
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		i++;
+		head = head->next;
+	}
+	return (NULL);
 }
