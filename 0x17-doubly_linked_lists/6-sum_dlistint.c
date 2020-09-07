587964bc682fcec9138f5ffbcf68ce299454de5f
silenarestrepo <restreposilena@gmail.com>
2020-09-07 03:12:57 +0000

    sum task dll

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,11 +1,19 @@
 #include "lists.h"
 
 /**
- * sum_dlistint -
- *
- * Return: 
+ * sum_dlistint -  sums all the data n of a dlinked list
+ * @head: head of the list
+ * Return: the sum of n
  */
 int sum_dlistint(dlistint_t *head)
 {
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 
 }
