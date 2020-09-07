bc2f05ea260911f0637284096a048c54b631880e
silenarestrepo <restreposilena@gmail.com>
2020-09-07 02:37:31 +0000

    free task dll

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,11 +1,18 @@
 #include "lists.h"
 
 /**
- * free_dlistint -
- *
- * Return: 
+ * free_dlistint - frees a dlinked list
+ * @head: head of the list
+ * Return: nothing
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *p;
 
+	while (head != NULL)
+	{
+		p = head->next;
+		free(head);
+		head = p;
+	}
 }
