0c1ba75875183a00021f5352b58f3db7c97f96f9
silenarestrepo <restreposilena@gmail.com>
2020-10-02 21:37:02 +0000

    task 1

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,10 @@
 #include "hash_tables.h"
-
+/**
+ * hash_djb2 - hash function with the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
 long int hash_djb2(const unsigned char *str)
 {
     unsigned long int hash;
