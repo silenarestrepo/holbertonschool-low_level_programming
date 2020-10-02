18192afa33336edeaf3d89b2c1cf47dd1db7b784
silenarestrepo <restreposilena@gmail.com>
2020-10-02 22:22:32 +0000

    task 2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -4,10 +4,10 @@
  * key_index - gives you the index of a key
  * @key: key
  * @size: size of the array of the hash table
- * Return: the index where the key should be stored in the array of the hash table
+ * Return: the index where the key should be stored in the array
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return (hash_djb2(key) % size);
+return (hash_djb2(key) % size);
 }
