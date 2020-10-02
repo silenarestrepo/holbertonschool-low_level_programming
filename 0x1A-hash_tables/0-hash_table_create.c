f3348346872be5df45543e007bfcf8e953882f68
silenarestrepo <restreposilena@gmail.com>
2020-10-02 21:30:49 +0000

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,19 +7,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *new;
+hash_table_t *new;
 
-    if (size == 0)
-        return (NULL);
-    new = malloc(sizeof(hash_table_t));
-    if (new == 0)
-        return (NULL);
-    new->array = malloc(sizeof(hash_node_t *) * size);
-    if (new->array == NULL)
-    {
-        free(new);
-        return(NULL);
-    }
-    new->size = size;
-    return (new);
+if (size == 0)
+return (NULL);
+new = malloc(sizeof(hash_table_t));
+if (new == 0)
+return (NULL);
+new->array = malloc(sizeof(hash_node_t *) * size);
+if (new->array == NULL)
+{
+free(new);
+return (NULL);
+}
+new->size = size;
+return (new);
 }
